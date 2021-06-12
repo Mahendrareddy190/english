66d8e97cb8bec4263d758e3ef0b72dae5b7290d9
Mahendrareddy190 <73093527+Mahendrareddy190@users.noreply.github.com>
2021-06-12 15:44:05 +0530

    Update comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -13,7 +13,7 @@ router.post("/create", Createcomment);
 router.get("/get/:CommentId", singleComment);
 router.get("/getall", getallcomments);
 router.delete("/delete/:CommentId", Deletecomment);
-router.get('test',(req,res)=>{
+router.get('/test',(req,res)=>{
   res.send({name:"mahi"})
 })
 
